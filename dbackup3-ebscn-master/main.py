fd6b2960ed6f64a5e05ba621acd21cab91c14f5c
zyy <zhangyingyi@scutech.com>
2023-11-10 13:15:05 +0800

    csv附件功能已在V44807版本测试完毕

--- dbackup3-ebscn-master/main.py
+++ dbackup3-ebscn-master/main.py
@@ -7,6 +7,8 @@ import smtplib
 import urllib
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timedelta
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from urllib import request, parse
 
@@ -18,6 +20,8 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
+START_TIME = str(datetime.utcnow() - timedelta(hours=24)).split(' ')[0]
+FILENAME='./tp.csv'
 
 html_template = """
 <html>
@@ -43,7 +47,6 @@ html_template = """
 </body>
 </html>
 """
-
 
 def transform_size(num):
     step = 1024
@@ -220,24 +223,33 @@ class EBSCN:
 
     def create_message(self, department):
         start_time = datetime.utcnow() - timedelta(hours=24)
+        job_overview_table=self.get_overview_table(department, start_time)
+        storage_pools_table=self.get_storage_pools_table()
+        job_histories_table=self.get_job_histories_table(department, start_time)
+        f = open(FILENAME, 'w', encoding='utf-8-sig')
+        for lst1 in job_overview_table[1]:
+            f.write(lst1 + "\n")
+        for lst2 in storage_pools_table[1]:
+            f.write(lst2 + "\n")
+        for lst3 in job_histories_table[1]:
+            f.write(lst3 + "\n")
         return html_template.format(
-            job_overview_table=self.get_overview_table(department, start_time),
-            storage_pools_table=self.get_storage_pools_table(),
-            job_histories_table=self.get_job_histories_table(
-                department, start_time
-            ),
+            job_overview_table=job_overview_table[0],
+            storage_pools_table=storage_pools_table[0],
+            job_histories_table=job_histories_table[0]
         )
 
     def get_job_histories_table(self, department, start_time):
         hosts = self.get_hosts()
         job_histories_csv = self.get_job_histories(department, start_time)
         job_histories = job_histories_csv.split("\n")
+        table_head = job_histories.pop(0).split(",")[0:-1]
         job_histories_table = [
             "\t\t<tr>\n\t\t\t<th>"
-            + "</th>\n\t\t\t<th>".join(job_histories.pop(0).split(",")[0:-1])
+            + "</th>\n\t\t\t<th>".join(table_head)
             + "</th>\n\t\t</tr>"
         ]
-
+        job_histories_table_csv = ["\n作业历史", ",".join(table_head)]
         for job in job_histories:
             if job:
                 job_row = job.split(",")
@@ -245,17 +257,21 @@ class EBSCN:
                     if host["uuid"] == job_row[-1]:
                         job_row[3] = host["address"]
                         break
-
+                    
                 job_histories_table.append(
                     "\t\t<tr>\n\t\t\t<td>"
                     + "</td>\n\t\t\t<td>".join(job_row[0:-1])
                     + "</td>\n\t\t</tr>"
                 )
-        return "\n".join(job_histories_table)
+                job_histories_table_csv.append(",".join(job_row[0:-1]))
+
+        return ["\n".join(job_histories_table), job_histories_table_csv]
 
     def get_storage_pools_table(self):
         storage_pools = self.get_storage_pools()
         storageds = self.get_storageds()
+        storage_pools_table_csv = ['\n存储池', "名称,类型,状态,可用,总共"]
+        storage_pools_row_csv = """{name},{type},{online},{free_size},{size}"""
         storage_pools_table = [
             """
         <tr>
@@ -276,7 +292,6 @@ class EBSCN:
             <td>{size}</td>
         </tr>
         """
-
         for pool in storage_pools["rows"]:
             storage = pool["storages"][0]
 
@@ -285,6 +300,17 @@ class EBSCN:
                     online = storaged["online"]
                     break
             try:
+                storage_pools_table_csv.append(
+                    storage_pools_row_csv.format(
+                        name=pool["name"],
+                        type=pool["type"],
+                        online="在线" if online else "离线",
+                        free_size=transform_size(
+                            self.get_storage_pool_free_size(storage)
+                        ),
+                        size=transform_size(storage["extended_attrs"]["size"]),
+                    )
+                )
                 storage_pools_table.append(
                     storage_pools_row.format(
                         name=pool["name"],
@@ -298,7 +324,7 @@ class EBSCN:
                 )
             except Exception as e:
                 logging.error(e)
-        return "".join(storage_pools_table)
+        return ["".join(storage_pools_table), storage_pools_table_csv]
 
     def get_storage_pool_free_size(self, storage):
         try:
@@ -327,6 +353,9 @@ class EBSCN:
             if total != 0
             else "-"
         )
+        tpstr1 = 'successful,'+str(successful_total)+','+str(successful_percentage)
+        tpstr2 = 'failure,'+str(failure_total)+','+str(failure_percentage)
+        job_overview_table_csv = ['作业概览', '状态,数量,百分比', tpstr1, tpstr2]
         job_overview_table = """
         <tr>
             <th>状态</th>
@@ -349,18 +378,21 @@ class EBSCN:
             failure_total=failure_total,
             failure_percentage=failure_percentage,
         )
-        return job_overview_table
+        return [job_overview_table, job_overview_table_csv]
 
-    def send_mail(self, sender, password, receivers, message, subject):
+    def send_mail(self, sender, password, receivers, htmlMsg, subject):
         try:
-            msg = MIMEText(message, "html", "utf-8")
+            msg = MIMEMultipart()
             msg["From"] = sender
             msg["To"] = receivers
             if subject:
                 msg["Subject"] = subject
             else:
                 msg["Subject"] = self._mail_subject
-
+            msg.attach(MIMEText(htmlMsg, "html", "utf-8"))
+            part = MIMEApplication(open(FILENAME, 'rb').read())
+            part.add_header('Content-Disposition', 'attachment', filename='dbackup_'+START_TIME+'.csv')
+            msg.attach(part)
             server = self.create_email_server()
             server.login(sender, password)
             server.sendmail(
---
+++ readme.md
@@ -0,0 +1,3 @@
+# 创建虚拟化作业示例
+1. 接口/ds/dbackup/:路由最后有/
+2. 该接口使用`'Content-Type': 'application/x-www-form-urlencoded',`注意提交参数格式
